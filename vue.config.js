41a9adb75ecc2cadbec4373c89860465cdce5de3
li.zhendong <li.zhendong@founder.com.cn>
2022-12-05 16:20:50 +0800

    修改跟路径

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  'css': {
-    'extract': false,
+  publicPath: 'mindmap',
+  css: {
+    extract: false,
   },
-  'transpileDependencies': [
+  transpileDependencies: [
     'vuetify',
   ],
   chainWebpack: config => {
@@ -10,8 +11,8 @@ module.exports = {
       .use('url-loader') // url-loader要搭配file-loader做图片压缩
       .loader('url-loader')
       .options({
-        limit: 1024 * 12,// 小于12kb的图片压缩成base64，图片太大转成base64反而不太合适
-        name: 'static/img/[name].[ext]'
+        limit: 1024 * 12, // 小于12kb的图片压缩成base64，图片太大转成base64反而不太合适
+        name: 'static/img/[name].[ext]',
         // 指定打包后的图片存放的位置，一般放在static下img文件夹里 name.ext分别为：文件名.文件后缀（按照原图片名）
       })
       .end() // 返回上一级 以便于继续添加loader
